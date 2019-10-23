f3d85a60a744f65c80e73e7a51db330b7c86d1bd
ismaelcarvalhorosa <dt65estudio@yahoo.com.br>
2019-10-23 14:29:16 -0300

    Rastro quanto enda,  tempo de um objto

--- Scripts/Carinha.gd
+++ Scripts/Carinha.gd
@@ -1,11 +1,17 @@
 tool
 extends KinematicBody2D
 
-var tiro_grupo = "tiro-" + str(self) # configura os gupos 
+onready var tiro_grupo = "tiro-" + str(self) # configura os gupos 
 
-var speed = 50 
+const ROT_VEL = PI
+
+const MAX_SPEED = 100 
 var andar = 4
-var pre_tiro = preload("res://cenas/Tiro_Area2D.tscn")
+var pre_tiro = preload("res://cenas/Tiro_Area2D.tscn") # preload carrega a variavel quanto o jogo é iniciado
+var pre_track = preload("res://cenas/Track.tscn") 
+
+var travel = 0
+var acell = 0
 
 export(int , "Revolver", "Espigarda cano curto cima", "revolver ouro") var arma = 1 setget set_arma
 
@@ -26,36 +32,70 @@ func _physics_process(delta):
 	if Engine.editor_hint:
 		return
 	
-	var dir_x = 0
-	var dir_y = 0
-	
-	if Input.is_action_pressed("ui_right"):
-		dir_x += andar
-	
-	if Input.is_action_pressed("ui_left"):
-		dir_x -= andar
-		
-	if Input.is_action_pressed("ui_up"):
-		dir_y -= andar
-	
-	if Input.is_action_pressed("ui_down"):
-		dir_y += andar
-		
+#	var dir_x = 0
+#	var dir_y = 0
+#
+#	if Input.is_action_pressed("ui_right"):
+#		dir_x += andar
+#
+#	if Input.is_action_pressed("ui_left"):
+#		dir_x -= andar
+#
+#	if Input.is_action_pressed("ui_up"):
+#		dir_y -= andar
+#
+#	if Input.is_action_pressed("ui_down"):
+#		dir_y += andar
+#
 	if Input.is_action_just_pressed("ui_shoot"):
-		
+
 		if get_tree().get_nodes_in_group(tiro_grupo).size() < 5:
 			var tiro = pre_tiro.instance()
 			tiro.global_position = $revolver/disparo.global_position
-			tiro.dir = Vector2(cos(rotation), sin(rotation)).normalized()
+			tiro.dir = Vector2(cos($revolver.global_rotation), sin($revolver.global_rotation)).normalized()
 			tiro.add_to_group(tiro_grupo)
 			$"../".add_child(tiro)
-			$revolver/AnimationPlayer.play("fire")
+#			$revolver/AnimationPlayer.play("fire")
+#
+#	look_at(get_global_mouse_position())
+#
+#	move_and_slide( Vector2(dir_x, dir_y) * speed)
 	
-	look_at(get_global_mouse_position())
+	var rot = 0
+	var dir = 0
+	
+	if Input.is_action_pressed("ui_right"):
+		rot += 1
 		
-	move_and_slide( Vector2(dir_x, dir_y) * speed)
+	if Input.is_action_pressed("ui_left"):
+		rot -= 1
+		
+	if Input.is_action_pressed("ui_up"):
+		dir += andar
+#
+	if Input.is_action_pressed("ui_down"):
+		dir -= andar
 	
-
+	rotate(ROT_VEL * rot * delta)
+	
+	acell = lerp(acell, MAX_SPEED * dir, .01)
+		
+	print(acell)
+	
+	var move = move_and_slide(Vector2(cos(rotation), sin(rotation)) * acell )  # Direção do vetor 
+	
+	travel += move.length() * delta
+	
+	if travel >= $"Chapel de cima".texture.get_size().y:
+		travel = 0
+		var track = pre_track.instance() 
+		track.global_position = global_position - Vector2(cos(rotation), sin(rotation)).normalized() * 5
+		track.rotation = rotation 
+		track.z_index = z_index - 1 
+		$"../".add_child(track)
+	
+	$revolver.look_at(get_global_mouse_position())
+	
 func set_arma(val):
 		arma = val
 		if Engine.editor_hint:
---
+++ Scripts/Track.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+func _on_Timer_queue_free_timeout():
+	queue_free()
